78590ea4af1c3194adaf0a5481defbf6f99abc61
supmatch <45656710+supmatch@users.noreply.github.com>
2019-05-08 16:02:26 +0800

    Update upload_client.py

--- upload_client.py
+++ upload_client.py
@@ -4,5 +4,10 @@ def upload_file(file_name):
     url = upload_url
     files = {'file':open(file_name, 'rb')}
     req = requests.post(url,files=files)
-    result = req.text
-    return result
+    data = req.text
+    return data
+
+if __name__ == '__main__':
+    filename = filename
+    data = upload_file(filename)
+    print(data)
